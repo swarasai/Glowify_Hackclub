0b6f2c2223b27a88711028b4753b016f9e1942cc
Akarsh228 <57933193+Akarsh228@users.noreply.github.com>
2024-02-03 13:19:08 -0800

    Initial Commit

---
+++ Glorify_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Glorify_App
+//
+//  Created by Akarsh Shetty on 2/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
